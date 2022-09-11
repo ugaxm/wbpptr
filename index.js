ff84d153c52af11ac123558352d6d3f963606b1a
VuaX <macroth@gmail.com>
2022-09-11 18:09:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const sendMessageToDiscord = (content) => {
 
     await new Promise(r => setTimeout(r, 5000));
    
-    const elements = await page.$$('.wb-item');
+    const elements = await page.$$('.card-main');
     // 过滤1小时内的微博
     const effWeiboItems = await Promise.all(elements.map(async (ele, index) => {
         const eleTimeEl = await ele.$('.time');
